5505881cb2158c5fba31d02235cd4628e00cf2eb
Avocodo-chp <annisayunia18@gmail.com>
2023-11-07 12:00:47 +0700

    route update

--- e_complaint/lib/views/Welcome/spalsh_screen.dart
+++ e_complaint/lib/views/Welcome/spalsh_screen.dart
@@ -1,1 +1,12 @@
 // dastin
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
