3d6bc07adac4f20a4e61c0106c9fd9e181b8b692
Avocodo-chp <annisayunia18@gmail.com>
2023-11-08 06:42:49 +0700

    Update login_screen.dart

--- e_complaint/lib/views/Login/login_screen.dart
+++ e_complaint/lib/views/Login/login_screen.dart
@@ -1,1 +1,11 @@
 // anisa
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
