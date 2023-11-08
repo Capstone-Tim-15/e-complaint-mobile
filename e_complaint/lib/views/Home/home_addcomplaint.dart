bdc73a33d2051869540aa1754b4e7344c04a87e5
mustika chairani <mustikachrn5@gmail.com>
2023-11-08 10:26:13 +0700

    Create home_addcomplaint.dart

---
+++ e_complaint/lib/views/Home/home_addcomplaint.dart
@@ -0,0 +1,17 @@
+//rani
+
+import 'package:flutter/material.dart';
+
+class AddComplaint extends StatefulWidget {
+  const AddComplaint({super.key});
+
+  @override
+  State<AddComplaint> createState() => _AddComplaintState();
+}
+
+class _AddComplaintState extends State<AddComplaint> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
