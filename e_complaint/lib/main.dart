9437b435a78bb99e249aac0814c7629422ba82ca
mustika chairani <mustikachrn5@gmail.com>
2023-11-08 10:19:08 +0700

    update

--- e_complaint/lib/main.dart
+++ e_complaint/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:e_complaint/views/Welcome_Screen/splash_screen.dart';
+import 'package:e_complaint/views/Welcome/spalsh_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
