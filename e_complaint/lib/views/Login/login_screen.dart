b83a66973884396b1098469c58f4016128a740d0
Avocodo-chp <annisayunia18@gmail.com>
2023-11-08 14:26:42 +0700

    update login

--- e_complaint/lib/views/Login/login_screen.dart
+++ e_complaint/lib/views/Login/login_screen.dart
@@ -1,11 +1,160 @@
-// anisa
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      useMaterial3: true,
+    ),
+    home: LoginPage(),
+  ));
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isPasswordVisible = false;
+  bool rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 265,
+                height: 88,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Halo!',
+                      style: TextStyle(
+                        color: Color(0xFF191C1D),
+                        fontSize: 57,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                        height: 0.02,
+                        letterSpacing: -0.25,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Container(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Belum mempunyai akun?',
+                            style: TextStyle(
+                              color: Color(0xFF191C1D),
+                              fontSize: 14,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w500,
+                              height: 0.10,
+                              letterSpacing: 0.10,
+                            ),
+                          ),
+                          Text(
+                            ' Buat akun disini',
+                            style: TextStyle(
+                              color: Color(0xFF001945),
+                              fontSize: 14,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w500,
+                              height: 0.10,
+                              letterSpacing: 0.10,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 15),
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isPasswordVisible = !isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: !isPasswordVisible,
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Checkbox(
+                    value: rememberMe,
+                    onChanged: (value) {
+                      setState(() {
+                        rememberMe = value!;
+                      });
+                    },
+                  ),
+                  Text('Ingat Saya'),
+                ],
+              ),
+              TextButton(
+                onPressed: () {
+                  // Implement 'Lupa Kata Sandi' logic here
+                },
+                child: Text('Lupa Kata Sandi'),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Implement login logic here
+                  },
+                  child: Text('Login'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
