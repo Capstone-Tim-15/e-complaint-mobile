ed70bdc797dd7de17fbf0b5bdb99e571446821fa
TrixTin23 <11201018@student.itk.ac.id>
2023-11-17 23:43:33 +0800

    fixed bg color

--- e_complaint/lib/views/Login/account_success.dart
+++ e_complaint/lib/views/Login/account_success.dart
@@ -14,61 +14,64 @@ class AccountSuccess extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 180,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Selamat!!",
-                  style: getTextStyle(),
-                ),
-                Text(
-                  "Akun anda berhasil dibuat",
-                  style: getTextStyle(),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            SizedBox(
-              width: 330,
-              child: Image.asset("assets/images/create_account_success.png"),
-            ),
-            const SizedBox(
-              height: 135,
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                onPressed: () {
-                  //Navigator
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFEC7B73),
-                    fixedSize: const Size(170, 45),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
+      body: Container(
+        color: Color(0xFFF8FAFA),
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 180,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Selamat!!",
+                    style: getTextStyle(),
+                  ),
+                  Text(
+                    "Akun anda berhasil dibuat",
+                    style: getTextStyle(),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: 330,
+                child: Image.asset("assets/images/create_account_success.png"),
+              ),
+              const SizedBox(
+                height: 135,
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    //Navigator
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFEC7B73),
+                      fixedSize: const Size(170, 45),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
                       ),
-                    ),
-                    textStyle: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      letterSpacing: 0.2,
-                    )),
-                child: const Text(
-                  "Halaman Utama",
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.2,
+                      )),
+                  child: const Text(
+                    "Halaman Utama",
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
