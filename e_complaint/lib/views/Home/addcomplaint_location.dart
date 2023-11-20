b475cc2053db2a089638978ba9c6807daf293b74
mustika chairani <mustikachrn5@gmail.com>
2023-11-20 15:29:50 +0700

    Create addcomplaint_location.dart

---
+++ e_complaint/lib/views/Home/addcomplaint_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComplaintLocation extends StatefulWidget {
+  const ComplaintLocation({super.key});
+
+  @override
+  State<ComplaintLocation> createState() => _ComplaintLocationState();
+}
+
+class _ComplaintLocationState extends State<ComplaintLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
