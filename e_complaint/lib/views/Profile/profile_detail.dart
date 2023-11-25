4a57330406a23838a0aa5cbd122afb8ad0e4688f
TrixTin23 <11201018@student.itk.ac.id>
2023-11-25 16:48:36 +0800

    add profile detail page

--- e_complaint/lib/views/Profile/profile_detail.dart
+++ e_complaint/lib/views/Profile/profile_detail.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const Apps());
+}
+
+class Apps extends StatelessWidget {
+  const Apps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Profiledetail(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
 
 class Profiledetail extends StatefulWidget {
   const Profiledetail({super.key});
@@ -8,14 +26,373 @@ class Profiledetail extends StatefulWidget {
 }
 
 class _ProfiledetailState extends State<Profiledetail> {
+  String coverImagePath = 'assets/images/news_image.jpg';
+  String profileImagePath = 'assets/images/user.png';
+
+  Future<void> _pickImage(ImageSource source, bool isCoverImage) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        if (isCoverImage) {
+          coverImagePath = pickedFile.path;
+        } else {
+          profileImagePath = pickedFile.path;
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile Detail'),
       ),
-      body: const Center(
-        child: Text('Profile Detail'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          // image
+          SizedBox(
+            width: double.infinity,
+            height: 239,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    _pickImage(ImageSource.gallery, true);
+                    print("Cover image ditekan, ubah gambar jika diperlukan");
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.grey,
+                      image: DecorationImage(
+                        image: AssetImage(coverImagePath),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _pickImage(ImageSource.gallery, false);
+                    print("Profile image ditekan, ubah gambar jika diperlukan");
+                  },
+                  child: Container(
+                    width: 90,
+                    height: 90,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      image: DecorationImage(
+                        image: AssetImage(profileImagePath),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    height: 18,
+                    width: 5000,
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                    child: const Text(
+                      "Tekan untuk ubah",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Gap(10),
+          // nama
+          Container(
+            height: 45,
+            padding:
+                const EdgeInsets.only(right: 16, left: 16, top: 10, bottom: 10),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Nama Lengkap",
+                  style: TextStyle(
+                    fontFamily: "Nunito",
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Nama",
+                      style: TextStyle(
+                        fontFamily: "Nunito",
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Gap(5),
+                    SizedBox(
+                      child: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const Divider(color: Colors.black),
+          //bio
+          Container(
+            height: 45,
+            padding:
+                const EdgeInsets.only(right: 16, left: 16, top: 10, bottom: 10),
+            child: const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Bio",
+                    style: TextStyle(
+                      fontFamily: "Nunito",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Bio",
+                        style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Gap(5),
+                      SizedBox(
+                        child: Icon(
+                          Icons.arrow_forward_ios_rounded,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(color: Colors.black),
+          //jenis
+          Container(
+            height: 45,
+            padding:
+                const EdgeInsets.only(right: 16, left: 16, top: 10, bottom: 10),
+            child: const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Jenis Kelamin",
+                    style: TextStyle(
+                      fontFamily: "Nunito",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Jenis",
+                        style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Gap(5),
+                      SizedBox(
+                        child: Icon(
+                          Icons.arrow_forward_ios_rounded,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(color: Colors.black),
+          //tgl lahir
+          Container(
+            height: 45,
+            padding:
+                const EdgeInsets.only(right: 16, left: 16, top: 10, bottom: 10),
+            child: const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Tanggal Lahir",
+                    style: TextStyle(
+                      fontFamily: "Nunito",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Tanggal",
+                        style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      // Gap(5),
+                      SizedBox(
+                        child: Icon(
+                          Icons.arrow_forward_ios_rounded,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(color: Colors.black),
+          //no hp
+          Container(
+            height: 45,
+            padding:
+                const EdgeInsets.only(right: 16, left: 16, top: 10, bottom: 10),
+            child: const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "No. Handphone",
+                    style: TextStyle(
+                      fontFamily: "Nunito",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Nomor",
+                        style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Gap(5),
+                      SizedBox(
+                        child: Icon(
+                          Icons.arrow_forward_ios_rounded,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(color: Colors.black),
+          //email
+          Container(
+            height: 45,
+            padding:
+                const EdgeInsets.only(right: 16, left: 16, top: 10, bottom: 10),
+            child: const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Email",
+                    style: TextStyle(
+                      fontFamily: "Nunito",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Email",
+                        style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Gap(5),
+                      SizedBox(
+                        child: Icon(
+                          Icons.arrow_forward_ios_rounded,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(color: Colors.black),
+          //alamat (jadi header)
+          Container(
+            height: 45,
+            padding:
+                const EdgeInsets.only(right: 16, left: 16, top: 10, bottom: 10),
+            child: const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Alamat",
+                    style: TextStyle(
+                      fontFamily: "Nunito",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    child: Icon(
+                      Icons.arrow_forward_ios_rounded,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(color: Colors.black),
+          //alamat
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 16, top: 10, right: 16),
+            child: const Text(
+              "Alamat",
+              style: TextStyle(
+                fontFamily: "Nunito",
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
