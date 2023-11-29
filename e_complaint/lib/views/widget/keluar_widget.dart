37d56dde475211d4c29a489051dbebffaf9dd071
ghazifarhanu <farhanuahan08@gmail.com>
2023-11-29 14:57:58 +0700

    profil_page

---
+++ e_complaint/lib/views/widget/keluar_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class keluar extends StatelessWidget {
+  const keluar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10, right: 20),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.deepOrangeAccent,
+          borderRadius: BorderRadius.circular(12.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: ListTile(
+          title: Center(
+            child: Text(
+              'Keluar',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          ),
+          onTap: () {},
+        ),
+      ),
+    );
+  }
+}
