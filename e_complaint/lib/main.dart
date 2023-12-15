cc2ee5554055e4c02002f5fba459f344482b849f
mustika chairani <mustikachrn5@gmail.com>
2023-12-16 06:08:37 +0700

    update faq_card

--- e_complaint/lib/main.dart
+++ e_complaint/lib/main.dart
@@ -68,7 +68,7 @@ class MyApp extends StatelessWidget {
         fontFamily: "Nunito",
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFDF2216)),
       ),
-      initialRoute: '/',
+      initialRoute: '/faq_page',
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case '/':
---
+++ e_complaint/lib/models/faq_card.dart
@@ -0,0 +1,15 @@
+class FAQCard {
+  final String id;
+  final String title;
+  final String content;
+  final String categoryId;
+
+  FAQCard({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.categoryId,
+  });
+
+  
+}
